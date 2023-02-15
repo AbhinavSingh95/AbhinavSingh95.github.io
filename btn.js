253c9b5da91b439a8fe400a37bd77021f061b5b2
Abhinav Singh <abhinav_singh95@hotmail.com>
2023-02-15 18:59:15 +1100

    Work Exp Section added

--- btn.js
+++ btn.js
@@ -4,3 +4,29 @@ $("button").click(function () {
         return text === "Sent!" ? "Send" : "Sent!";
     });
 });
+
+const tabs = document.querySelector(".tabs");
+
+tabs.addEventListener("click", (e) => handleClick(e));
+
+function handleClick(e) {
+    e.preventDefault();
+    const target = e.target.closest("div");
+    const tabNum = target.dataset.tab;
+    const activeTab = document.querySelector(".tabs .active");
+    const activeContent = document.querySelector(".content .visible");
+    const currentContent = document.querySelector(
+        `.content__section[data-tab='${tabNum}']`
+    );
+
+    if (!tabNum) {
+        return;
+    }
+
+    activeTab.classList.remove("active");
+    activeTab.classList.remove("selected");
+    target.classList.add("active");
+    target.classList.add("selected");
+    activeContent.classList.remove("visible");
+    currentContent.classList.add("visible");
+}
